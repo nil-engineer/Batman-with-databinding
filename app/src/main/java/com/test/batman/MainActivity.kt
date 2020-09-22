f371d1b6af7924780f086aa8fbfa0604fc9539a4
shabestari <niloufar2012shabestari@gmail.com>
2020-09-22 23:35:33 +0330

    Initial commit

---
+++ app/src/main/java/com/test/batman/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.test.batman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
